8a9765585e4da795d62e1a429bc75824867af02f
Dalavai Devarajulu <devaraju@xilinx.com>
2019-08-01 11:24:27 +0530

    Uploading AC701 Application preset example design
    Uploading AC701 Application preset example design

---
+++ ced/Xilinx/IPI/MicroBlaze_Application_Configuration_for_AC701_design/1.0/init.tcl
@@ -0,0 +1,37 @@
+set currentFile [file normalize [info script]] 
+set currentDir [file dirname $currentFile] 
+proc getConfigDesignInfo {} { 
+  return [dict create name {MicroBlaze_Application_Configuration_for_AC701} description {}]
+}
+
+proc getSupportedParts {} { 
+}
+
+proc getSupportedBoards {} { 
+  return [list xilinx.com:ac701:1.3]
+}
+
+#proc addOptions {DESIGNOBJ} { 
+  #This proc is intended for declaring configurable options for this design
+#}
+
+#proc addGUILayout {DESIGNOBJ} { 
+  #This proc is intended for gui layout information of configurable options
+#}
+
+#DO NOT MODIFY THIS PROC 
+proc isGeneratedFromWriteProjectTcl {DESIGNOBJ} { 
+  return true 
+}
+
+proc createDesign { project_name {project_location "."} {options ""}} { 
+  variable currentDir
+  set ::user_project_name $project_name
+  set curr_location [pwd]
+  cd $project_location
+  #set ::user_project_location $project_location
+  catch {source -notrace "$currentDir/MicroBlaze_Application_Configuration_for_AC701_design.tcl"} retString 
+
+  cd $curr_location
+}
+
