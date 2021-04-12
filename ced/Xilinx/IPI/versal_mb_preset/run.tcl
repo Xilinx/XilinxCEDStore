b4ac435fd216e5ebd679def1aa7f2c13d76aea76
Siva Kemidi <skemidi@xlnx.xilinx.com>
2021-04-12 17:11:49 +0530

    Update run.tcl
    updated support for vpk120

--- ced/Xilinx/IPI/versal_mb_preset/run.tcl
+++ ced/Xilinx/IPI/versal_mb_preset/run.tcl
@@ -37,11 +37,12 @@ puts "INFO: $board_part is selected"
 puts "INFO: $fpga_part is selected"
 
 create_bd_cell -type ip -vlnv xilinx.com:ip:versal_cips:* versal_cips_0
-set_property -dict [list CONFIG.PS_PMC_CONFIG { PS_MIO7 {{AUX_IO 0} {DIRECTION in} {DRIVE_STRENGTH 8mA} {OUTPUT_DATA high} {PULL disable} {SCHMITT 0} {SLEW slow} {USAGE Unassigned}} PS_MIO9 {{AUX_IO 0} {DIRECTION in} {DRIVE_STRENGTH 8mA} {OUTPUT_DATA high} {PULL disable} {SCHMITT 0} {SLEW slow} {USAGE Unassigned}} PS_MIO19 {{AUX_IO 0} {DIRECTION in} {DRIVE_STRENGTH 8mA} {OUTPUT_DATA high} {PULL disable} {SCHMITT 0} {SLEW slow} {USAGE Unassigned}} PS_MIO21 {{AUX_IO 0} {DIRECTION in} {DRIVE_STRENGTH 8mA} {OUTPUT_DATA high} {PULL disable} {SCHMITT 0} {SLEW slow} {USAGE Unassigned}} PMC_MIO37 {{AUX_IO 0} {DIRECTION out} {DRIVE_STRENGTH 8mA} {OUTPUT_DATA high} {PULL pullup} {SCHMITT 0} {SLEW slow} {USAGE GPIO}} PMC_SD1 {{CD_ENABLE 1} {CD_IO {PMC_MIO 28}} {POW_ENABLE 1} {POW_IO {PMC_MIO 51}} {RESET_ENABLE 0} {RESET_IO {PMC_MIO 1}} {WP_ENABLE 0} {WP_IO PMC_MIO 1}} PMC_SD1_COHERENCY 0 PMC_SD1_DATA_TRANSFER_MODE 8Bit PMC_SD1_PERIPHERAL {{ENABLE 1} {IO {PMC_MIO 26 .. 36}}} PMC_SD1_SLOT_TYPE {SD 3.0} PMC_GPIO0_MIO_PERIPHERAL {{ENABLE 1} {IO {PMC_MIO 0 .. 25}}} PMC_GPIO1_MIO_PERIPHERAL {{ENABLE 1} {IO {PMC_MIO 26 .. 51}}} PMC_I2CPMC_PERIPHERAL {{ENABLE 1} {IO {PMC_MIO 46 .. 47}}} PMC_OSPI_PERIPHERAL {{ENABLE 0} {IO {PMC_MIO 0 .. 11}}  {MODE Single}} PMC_QSPI_COHERENCY 0 PMC_QSPI_FBCLK {{ENABLE 1} {IO {PMC_MIO 6}}} PMC_QSPI_PERIPHERAL_DATA_MODE x4 PMC_QSPI_PERIPHERAL_ENABLE 1 PMC_QSPI_PERIPHERAL_MODE {Dual Parallel} PS_CAN1_PERIPHERAL {{ENABLE 1} {IO {PMC_MIO 40 .. 41}}} PS_ENET0_MDIO {{ENABLE 1} {IO {PS_MIO 24 .. 25}}} PS_ENET0_PERIPHERAL {{ENABLE 1} {IO {PS_MIO 0 .. 11}}} PS_ENET1_PERIPHERAL {{ENABLE 1} {IO {PS_MIO 12 .. 23}}} PS_GEN_IPI0_ENABLE 1 PS_GEN_IPI0_MASTER A72 PS_GEN_IPI1_ENABLE 1 PS_GEN_IPI1_MASTER A72 PS_GEN_IPI2_ENABLE 1 PS_GEN_IPI2_MASTER A72 PS_GEN_IPI3_ENABLE 1 PS_GEN_IPI3_MASTER A72 PS_GEN_IPI4_ENABLE 1 PS_GEN_IPI4_MASTER A72 PS_GEN_IPI5_ENABLE 1 PS_GEN_IPI5_MASTER A72 PS_GEN_IPI6_ENABLE 1 PS_GEN_IPI6_MASTER A72 PS_GEN_IPI_PMCNOBUF_ENABLE 1 PS_GEN_IPI_PMC_ENABLE 1 PS_GEN_IPI_PSM_ENABLE 1 PS_I2C1_PERIPHERAL {{ENABLE 1} {IO {PMC_MIO 44 .. 45}}} PS_TTC3_PERIPHERAL_ENABLE 0  PS_UART0_PERIPHERAL {{ENABLE 1} {IO {PMC_MIO 42 .. 43}}} PS_USB3_PERIPHERAL {{ENABLE 1} {IO {PMC_MIO 13 .. 25}}} PMC_REF_CLK_FREQMHZ 33.333333} CONFIG.PS_PMC_CONFIG_APPLIED {1} CONFIG.PS_BOARD_INTERFACE {cips_fixed_io}] [get_bd_cells versal_cips_0]
+#set_property -dict [list CONFIG.PS_PMC_CONFIG { PS_MIO7 {{AUX_IO 0} {DIRECTION in} {DRIVE_STRENGTH 8mA} {OUTPUT_DATA high} {PULL disable} {SCHMITT 0} {SLEW slow} {USAGE Unassigned}} PS_MIO9 {{AUX_IO 0} {DIRECTION in} {DRIVE_STRENGTH 8mA} {OUTPUT_DATA high} {PULL disable} {SCHMITT 0} {SLEW slow} {USAGE Unassigned}} PS_MIO19 {{AUX_IO 0} {DIRECTION in} {DRIVE_STRENGTH 8mA} {OUTPUT_DATA high} {PULL disable} {SCHMITT 0} {SLEW slow} {USAGE Unassigned}} PS_MIO21 {{AUX_IO 0} {DIRECTION in} {DRIVE_STRENGTH 8mA} {OUTPUT_DATA high} {PULL disable} {SCHMITT 0} {SLEW slow} {USAGE Unassigned}} PMC_MIO37 {{AUX_IO 0} {DIRECTION out} {DRIVE_STRENGTH 8mA} {OUTPUT_DATA high} {PULL pullup} {SCHMITT 0} {SLEW slow} {USAGE GPIO}} PMC_SD1 {{CD_ENABLE 1} {CD_IO {PMC_MIO 28}} {POW_ENABLE 1} {POW_IO {PMC_MIO 51}} {RESET_ENABLE 0} {RESET_IO {PMC_MIO 1}} {WP_ENABLE 0} {WP_IO PMC_MIO 1}} PMC_SD1_COHERENCY 0 PMC_SD1_DATA_TRANSFER_MODE 8Bit PMC_SD1_PERIPHERAL {{ENABLE 1} {IO {PMC_MIO 26 .. 36}}} PMC_SD1_SLOT_TYPE {SD 3.0} PMC_GPIO0_MIO_PERIPHERAL {{ENABLE 1} {IO {PMC_MIO 0 .. 25}}} PMC_GPIO1_MIO_PERIPHERAL {{ENABLE 1} {IO {PMC_MIO 26 .. 51}}} PMC_I2CPMC_PERIPHERAL {{ENABLE 1} {IO {PMC_MIO 46 .. 47}}} PMC_OSPI_PERIPHERAL {{ENABLE 0} {IO {PMC_MIO 0 .. 11}}  {MODE Single}} PMC_QSPI_COHERENCY 0 PMC_QSPI_FBCLK {{ENABLE 1} {IO {PMC_MIO 6}}} PMC_QSPI_PERIPHERAL_DATA_MODE x4 PMC_QSPI_PERIPHERAL_ENABLE 1 PMC_QSPI_PERIPHERAL_MODE {Dual Parallel} PS_CAN1_PERIPHERAL {{ENABLE 1} {IO {PMC_MIO 40 .. 41}}} PS_ENET0_MDIO {{ENABLE 1} {IO {PS_MIO 24 .. 25}}} PS_ENET0_PERIPHERAL {{ENABLE 1} {IO {PS_MIO 0 .. 11}}} PS_ENET1_PERIPHERAL {{ENABLE 1} {IO {PS_MIO 12 .. 23}}} PS_GEN_IPI0_ENABLE 1 PS_GEN_IPI0_MASTER A72 PS_GEN_IPI1_ENABLE 1 PS_GEN_IPI1_MASTER A72 PS_GEN_IPI2_ENABLE 1 PS_GEN_IPI2_MASTER A72 PS_GEN_IPI3_ENABLE 1 PS_GEN_IPI3_MASTER A72 PS_GEN_IPI4_ENABLE 1 PS_GEN_IPI4_MASTER A72 PS_GEN_IPI5_ENABLE 1 PS_GEN_IPI5_MASTER A72 PS_GEN_IPI6_ENABLE 1 PS_GEN_IPI6_MASTER A72 PS_GEN_IPI_PMCNOBUF_ENABLE 1 PS_GEN_IPI_PMC_ENABLE 1 PS_GEN_IPI_PSM_ENABLE 1 PS_I2C1_PERIPHERAL {{ENABLE 1} {IO {PMC_MIO 44 .. 45}}} PS_TTC3_PERIPHERAL_ENABLE 0  PS_UART0_PERIPHERAL {{ENABLE 1} {IO {PMC_MIO 42 .. 43}}} PS_USB3_PERIPHERAL {{ENABLE 1} {IO {PMC_MIO 13 .. 25}}} PMC_REF_CLK_FREQMHZ 33.333333} CONFIG.PS_PMC_CONFIG_APPLIED {1} CONFIG.PS_BOARD_INTERFACE {cips_fixed_io}] [get_bd_cells versal_cips_0]
+apply_board_connection -board_interface "cips_fixed_io" -ip_intf "versal_cips_0/FIXED_IO" -diagram $design_name 
 set_property -dict [list CONFIG.PS_PMC_CONFIG { CLOCK_MODE Custom PS_NUM_FABRIC_RESETS 1 PS_PL_CONNECTIVITY_MODE Custom PS_USE_PMCPL_CLK0 1 PS_USE_S_AXI_LPD 1 PS_USE_BSCAN_USER2 1} CONFIG.CLOCK_MODE {Custom} CONFIG.PS_PL_CONNECTIVITY_MODE {Custom}] [get_bd_cells versal_cips_0]
 
 create_bd_cell -type ip -vlnv xilinx.com:ip:clk_wizard:* clk_wizard_0
-set_property -dict [list CONFIG.PRIM_SOURCE {No_buffer} CONFIG.USE_RESET {true} CONFIG.RESET_TYPE {ACTIVE_LOW} CONFIG.CLKOUT_USED {true,false,false,false,false,false,false} CONFIG.CLKOUT_PORT {clk_out1,clk_out2,clk_out3,clk_out4,clk_out5,clk_out6,clk_out7} CONFIG.CLKOUT_REQUESTED_OUT_FREQUENCY {200,100.000,100.000,100.000,100.000,100.000,100.000} CONFIG.CLKOUT_REQUESTED_PHASE {0.000,0.000,0.000,0.000,0.000,0.000,0.000} CONFIG.CLKOUT_REQUESTED_DUTY_CYCLE {50.000,50.000,50.000,50.000,50.000,50.000,50.000} CONFIG.CLKOUT_DRIVES {BUFG,BUFG,BUFG,BUFG,BUFG,BUFG,BUFG} CONFIG.CLKOUT_GROUPING {Auto,Auto,Auto,Auto,Auto,Auto,Auto} CONFIG.CLKOUT_DYN_PS {None,None,None,None,None,None,None} CONFIG.CLKOUT_MATCHED_ROUTING {false,false,false,false,false,false,false} CONFIG.CLKOUT1_DIVIDE {12.000000}] [get_bd_cells clk_wizard_0]
+set_property -dict [list CONFIG.PRIM_SOURCE {No_buffer} CONFIG.USE_RESET {true} CONFIG.RESET_TYPE {ACTIVE_LOW} CONFIG.CLKOUT_USED {true,false,false,false,false,false,false} CONFIG.CLKOUT_PORT {clk_out1,clk_out2,clk_out3,clk_out4,clk_out5,clk_out6,clk_out7} CONFIG.CLKOUT_REQUESTED_OUT_FREQUENCY {200,100.000,100.000,100.000,100.000,100.000,100.000} CONFIG.CLKOUT_REQUESTED_PHASE {0.000,0.000,0.000,0.000,0.000,0.000,0.000} CONFIG.CLKOUT_REQUESTED_DUTY_CYCLE {50.000,50.000,50.000,50.000,50.000,50.000,50.000} CONFIG.CLKOUT_DRIVES {BUFG,BUFG,BUFG,BUFG,BUFG,BUFG,BUFG} CONFIG.CLKOUT_GROUPING {Auto,Auto,Auto,Auto,Auto,Auto,Auto} CONFIG.CLKOUT_DYN_PS {None,None,None,None,None,None,None} CONFIG.CLKOUT_MATCHED_ROUTING {false,false,false,false,false,false,false} ] [get_bd_cells clk_wizard_0]
 apply_bd_automation -rule xilinx.com:bd_rule:board -config { Clk {/versal_cips_0/pl0_ref_clk (333 MHz)} Manual_Source {Auto}}  [get_bd_pins clk_wizard_0/clk_in1]
 apply_bd_automation -rule xilinx.com:bd_rule:board -config { Manual_Source {/versal_cips_0/pl0_resetn (ACTIVE_LOW)}}  [get_bd_pins clk_wizard_0/resetn]
 
@@ -49,19 +50,36 @@ create_bd_cell -type ip -vlnv xilinx.com:ip:microblaze:* microblaze_0
 
 create_bd_cell -type ip -vlnv xilinx.com:ip:axi_timer:* axi_timer_0
 
-create_bd_cell -type ip -vlnv xilinx.com:ip:axi_gpio axi_gpio_0
-apply_board_connection -board_interface "dc_pl_gpio" -ip_intf "axi_gpio_0/GPIO" -diagram $design_name 
-apply_board_connection -board_interface "gpio_dp" -ip_intf "/axi_gpio_0/GPIO2" -diagram $design_name 
+set gpios_list ""
+set gpios_list [ get_board_component_interfaces -filter {BUSDEF_NAME == gpio_rtl} ]
+	set gpio_cnt 0
+	foreach gpio $gpios_list {
+		set gpio_cnt [expr $gpio_cnt + 1]
+		if { $gpio_cnt % 2 == 1} {
+			create_bd_cell -type ip -vlnv xilinx.com:ip:axi_gpio axi_gpio_[expr $gpio_cnt/2]
+			apply_board_connection -board_interface "$gpio" -ip_intf "axi_gpio_[expr $gpio_cnt/2]/GPIO" -diagram $design_name
+			} else {
+			apply_board_connection -board_interface "$gpio" -ip_intf "axi_gpio_[expr (($gpio_cnt - 1)/2)]/GPIO2" -diagram $design_name
+			}
+	}
 
-create_bd_cell -type ip -vlnv xilinx.com:ip:axi_gpio:* axi_gpio_1
-apply_board_connection -board_interface "gpio_led" -ip_intf "axi_gpio_1/GPIO" -diagram $design_name 
-apply_board_connection -board_interface "gpio_pb" -ip_intf "/axi_gpio_1/GPIO2" -diagram $design_name 
+# create_bd_cell -type ip -vlnv xilinx.com:ip:axi_gpio axi_gpio_0
+# apply_board_connection -board_interface "dc_pl_gpio" -ip_intf "axi_gpio_0/GPIO" -diagram $design_name 
+# apply_board_connection -board_interface "gpio_dp" -ip_intf "/axi_gpio_0/GPIO2" -diagram $design_name 
 
-create_bd_cell -type ip -vlnv xilinx.com:ip:axi_gpio:* axi_gpio_2
-apply_board_connection -board_interface "sysctlr_gpio" -ip_intf "axi_gpio_2/GPIO" -diagram $design_name 
+# create_bd_cell -type ip -vlnv xilinx.com:ip:axi_gpio:* axi_gpio_1
+# apply_board_connection -board_interface "gpio_led" -ip_intf "axi_gpio_1/GPIO" -diagram $design_name 
+# apply_board_connection -board_interface "gpio_pb" -ip_intf "/axi_gpio_1/GPIO2" -diagram $design_name 
+
+# create_bd_cell -type ip -vlnv xilinx.com:ip:axi_gpio:* axi_gpio_2
+# apply_board_connection -board_interface "sysctlr_gpio" -ip_intf "axi_gpio_2/GPIO" -diagram $design_name 
 
 create_bd_cell -type ip -vlnv xilinx.com:ip:axi_uart16550:* axi_uart16550_0
-apply_board_connection -board_interface "uart2_bank306" -ip_intf "axi_uart16550_0/UART" -diagram $design_name 
+
+if {[regexp vpk120 $board_name] } {
+apply_board_connection -board_interface "uart2_bank712" -ip_intf "axi_uart16550_0/UART" -diagram $design_name 
+} else {
+apply_board_connection -board_interface "uart2_bank306" -ip_intf "axi_uart16550_0/UART" -diagram $design_name }
 
 if {([lsearch $temp_options Preset.VALUE] == -1) || ([lsearch $temp_options "Microcontroller"] != -1)} {
 	puts "INFO: Microcontroller preset enabled"
@@ -89,8 +107,14 @@ if {([lsearch $temp_options Preset.VALUE] == -1) || ([lsearch $temp_options "Mic
 	set_property -dict [list CONFIG.PS_PMC_CONFIG { DDR_MEMORY_MODE Custom PMC_USE_PMC_NOC_AXI0 1} CONFIG.DDR_MEMORY_MODE {Custom}] [get_bd_cells versal_cips_0]
 	
 	create_bd_cell -type ip -vlnv xilinx.com:ip:axi_noc:* axi_noc_0
+		
+	if {[regexp vpk120 $board_name] } {
+	apply_board_connection -board_interface "ch0_lpddr4_trip1" -ip_intf "axi_noc_0/CH0_LPDDR4_0" -diagram $design_name 
+	apply_board_connection -board_interface "ch1_lpddr4_trip1" -ip_intf "axi_noc_0/CH1_LPDDR4_0" -diagram $design_name 
+	apply_board_connection -board_interface "lpddr4_clk1" -ip_intf "axi_noc_0/sys_clk0" -diagram $design_name 
+	} else {
 	apply_board_connection -board_interface "ddr4_dimm1" -ip_intf "axi_noc_0/CH0_DDR4_0" -diagram $design_name 
-	apply_board_connection -board_interface "ddr4_dimm1_sma_clk" -ip_intf "axi_noc_0/sys_clk0" -diagram $design_name 
+	apply_board_connection -board_interface "ddr4_dimm1_sma_clk" -ip_intf "axi_noc_0/sys_clk0" -diagram $design_name }
 	
 	set_property -dict [list CONFIG.NUM_SI {2} CONFIG.NUM_MI {0} CONFIG.NUM_CLKS {2} CONFIG.MC_CHAN_REGION1 {DDR_LOW1}] [get_bd_cells axi_noc_0]
 	set_property -dict [list CONFIG.CONNECTIONS {MC_0 { read_bw {1720} write_bw {1720} read_avg_burst {4} write_avg_burst {4}} }] [get_bd_intf_pins /axi_noc_0/S00_AXI]
@@ -161,6 +185,25 @@ if {([lsearch $temp_options Preset.VALUE] == -1) || ([lsearch $temp_options "Mic
 	 
 } else {
 	puts "ERROR: INVALID PRESET OPTION SELECTED" }
+if {[regexp vpk120 $board_name] } {
+set_property -dict [list CONFIG.NUM_PORTS {4}] [get_bd_cells microblaze_0_xlconcat] 
+} else {
+set_property -dict [list CONFIG.NUM_PORTS {5}] [get_bd_cells microblaze_0_xlconcat] }
+
+set gpios_list ""
+set gpios_list [ get_board_component_interfaces -filter {BUSDEF_NAME == gpio_rtl} ]
+	set gpio_cnt 0
+	set i 2
+	foreach gpio $gpios_list {
+		set gpio_cnt [expr $gpio_cnt + 1]
+		if { $gpio_cnt % 2 == 1} {
+			#create_bd_cell -type ip -vlnv xilinx.com:ip:axi_gpio axi_gpio_[expr $gpio_cnt/2]
+			apply_bd_automation -rule xilinx.com:bd_rule:axi4 -config {Master "/microblaze_0 (Periph)" Clk "Auto" }  [get_bd_intf_pins axi_gpio_[expr $gpio_cnt/2]/S_AXI]
+			#apply_board_connection -board_interface "$gpio" -ip_intf "axi_gpio_[expr $gpio_cnt/2]/GPIO" -diagram $design_name
+			set_property -dict [list CONFIG.C_INTERRUPT_PRESENT {1}] [get_bd_cells axi_gpio_[expr $gpio_cnt/2]]
+			connect_bd_net [get_bd_pins axi_gpio_[expr $gpio_cnt/2]/ip2intc_irpt] [get_bd_pins microblaze_0_xlconcat/In$i]
+			incr i
+			} }
 	
 # apply_bd_automation -rule xilinx.com:bd_rule:board -config { Manual_Source {/versal_cips_0/pl0_resetn (ACTIVE_LOW)}}  [get_bd_pins rst_versal_cips_0_249M/ext_reset_in]
 # apply_bd_automation -rule xilinx.com:bd_rule:axi4 -config { Clk_master {/versal_cips_0/pl0_ref_clk (249 MHz)} Clk_slave {Auto} Clk_xbar {/versal_cips_0/pl0_ref_clk (249 MHz)} Master {/microblaze_0 (Periph)} Slave {/axi_gpio_0/S_AXI} ddr_seg {Auto} intc_ip {/microblaze_0_axi_periph} master_apm {0}}  [get_bd_intf_pins axi_gpio_0/S_AXI]
@@ -170,23 +213,23 @@ if {([lsearch $temp_options Preset.VALUE] == -1) || ([lsearch $temp_options "Mic
 # apply_bd_automation -rule xilinx.com:bd_rule:axi4 -config { Clk_master {/versal_cips_0/pl0_ref_clk (249 MHz)} Clk_slave {Auto} Clk_xbar {/versal_cips_0/pl0_ref_clk (249 MHz)} Master {/microblaze_0 (Periph)} Slave {/axi_timer_0/S_AXI} ddr_seg {Auto} intc_ip {/microblaze_0_axi_periph} master_apm {0}}  [get_bd_intf_pins axi_timer_0/S_AXI]
 # apply_bd_automation -rule xilinx.com:bd_rule:axi4 -config { Clk_master {/versal_cips_0/pl0_ref_clk (249 MHz)} Clk_slave {Auto} Clk_xbar {/versal_cips_0/pl0_ref_clk (249 MHz)} Master {/microblaze_0 (Periph)} Slave {/axi_uart16550_0/S_AXI} ddr_seg {Auto} intc_ip {/microblaze_0_axi_periph} master_apm {0}}  [get_bd_intf_pins axi_uart16550_0/S_AXI]
 
-apply_bd_automation -rule xilinx.com:bd_rule:axi4 -config { Clk_master {/clk_wizard_0/clk_out1 (200 MHz)} Clk_slave {Auto} Clk_xbar {/clk_wizard_0/clk_out1 (200 MHz)} Master {/microblaze_0 (Periph)} Slave {/axi_gpio_0/S_AXI} ddr_seg {Auto} intc_ip {/microblaze_0_axi_periph} master_apm {0}}  [get_bd_intf_pins axi_gpio_0/S_AXI]
-apply_bd_automation -rule xilinx.com:bd_rule:axi4 -config { Clk_master {/clk_wizard_0/clk_out1 (200 MHz)} Clk_slave {Auto} Clk_xbar {/clk_wizard_0/clk_out1 (200 MHz)} Master {/microblaze_0 (Periph)} Slave {/axi_gpio_1/S_AXI} ddr_seg {Auto} intc_ip {/microblaze_0_axi_periph} master_apm {0}}  [get_bd_intf_pins axi_gpio_1/S_AXI]
-apply_bd_automation -rule xilinx.com:bd_rule:axi4 -config { Clk_master {/clk_wizard_0/clk_out1 (200 MHz)} Clk_slave {Auto} Clk_xbar {/clk_wizard_0/clk_out1 (200 MHz)} Master {/microblaze_0 (Periph)} Slave {/axi_gpio_2/S_AXI} ddr_seg {Auto} intc_ip {/microblaze_0_axi_periph} master_apm {0}}  [get_bd_intf_pins axi_gpio_2/S_AXI]
+#apply_bd_automation -rule xilinx.com:bd_rule:axi4 -config { Clk_master {/clk_wizard_0/clk_out1 (200 MHz)} Clk_slave {Auto} Clk_xbar {/clk_wizard_0/clk_out1 (200 MHz)} Master {/microblaze_0 (Periph)} Slave {/axi_gpio_0/S_AXI} ddr_seg {Auto} intc_ip {/microblaze_0_axi_periph} master_apm {0}}  [get_bd_intf_pins axi_gpio_0/S_AXI]
+#apply_bd_automation -rule xilinx.com:bd_rule:axi4 -config { Clk_master {/clk_wizard_0/clk_out1 (200 MHz)} Clk_slave {Auto} Clk_xbar {/clk_wizard_0/clk_out1 (200 MHz)} Master {/microblaze_0 (Periph)} Slave {/axi_gpio_1/S_AXI} ddr_seg {Auto} intc_ip {/microblaze_0_axi_periph} master_apm {0}}  [get_bd_intf_pins axi_gpio_1/S_AXI]
+#apply_bd_automation -rule xilinx.com:bd_rule:axi4 -config { Clk_master {/clk_wizard_0/clk_out1 (200 MHz)} Clk_slave {Auto} Clk_xbar {/clk_wizard_0/clk_out1 (200 MHz)} Master {/microblaze_0 (Periph)} Slave {/axi_gpio_2/S_AXI} ddr_seg {Auto} intc_ip {/microblaze_0_axi_periph} master_apm {0}}  [get_bd_intf_pins axi_gpio_2/S_AXI]
 apply_bd_automation -rule xilinx.com:bd_rule:axi4 -config { Clk_master {/clk_wizard_0/clk_out1 (200 MHz)} Clk_slave {Auto} Clk_xbar {/clk_wizard_0/clk_out1 (200 MHz)} Master {/microblaze_0 (Periph)} Slave {/axi_timer_0/S_AXI} ddr_seg {Auto} intc_ip {/microblaze_0_axi_periph} master_apm {0}}  [get_bd_intf_pins axi_timer_0/S_AXI]
 apply_bd_automation -rule xilinx.com:bd_rule:axi4 -config { Clk_master {/clk_wizard_0/clk_out1 (200 MHz)} Clk_slave {Auto} Clk_xbar {/clk_wizard_0/clk_out1 (200 MHz)} Master {/microblaze_0 (Periph)} Slave {/axi_uart16550_0/S_AXI} ddr_seg {Auto} intc_ip {/microblaze_0_axi_periph} master_apm {0}}  [get_bd_intf_pins axi_uart16550_0/S_AXI]
 apply_bd_automation -rule xilinx.com:bd_rule:axi4 -config { Clk_master {/clk_wizard_0/clk_out1 (200 MHz)} Clk_slave {Auto} Clk_xbar {/clk_wizard_0/clk_out1 (200 MHz)} Master {/microblaze_0 (Periph)} Slave {/versal_cips_0/S_AXI_LPD} ddr_seg {Auto} intc_ip {/microblaze_0_axi_periph} master_apm {0}}  [get_bd_intf_pins versal_cips_0/S_AXI_LPD]
 
-set_property -dict [list CONFIG.C_INTERRUPT_PRESENT {1}] [get_bd_cells axi_gpio_0]
-set_property -dict [list CONFIG.C_INTERRUPT_PRESENT {1}] [get_bd_cells axi_gpio_1]
-set_property -dict [list CONFIG.C_INTERRUPT_PRESENT {1}] [get_bd_cells axi_gpio_2]
+#set_property -dict [list CONFIG.C_INTERRUPT_PRESENT {1}] [get_bd_cells axi_gpio_0]
+#set_property -dict [list CONFIG.C_INTERRUPT_PRESENT {1}] [get_bd_cells axi_gpio_1]
+#set_property -dict [list CONFIG.C_INTERRUPT_PRESENT {1}] [get_bd_cells axi_gpio_2]
 
-set_property -dict [list CONFIG.NUM_PORTS {5}] [get_bd_cells microblaze_0_xlconcat]
+#set_property -dict [list CONFIG.NUM_PORTS {5}] [get_bd_cells microblaze_0_xlconcat]
 connect_bd_net [get_bd_pins axi_timer_0/interrupt] [get_bd_pins microblaze_0_xlconcat/In0]
 connect_bd_net [get_bd_pins axi_uart16550_0/ip2intc_irpt] [get_bd_pins microblaze_0_xlconcat/In1]
-connect_bd_net [get_bd_pins axi_gpio_0/ip2intc_irpt] [get_bd_pins microblaze_0_xlconcat/In2]
-connect_bd_net [get_bd_pins axi_gpio_1/ip2intc_irpt] [get_bd_pins microblaze_0_xlconcat/In3]
-connect_bd_net [get_bd_pins axi_gpio_2/ip2intc_irpt] [get_bd_pins microblaze_0_xlconcat/In4]
+# connect_bd_net [get_bd_pins axi_gpio_0/ip2intc_irpt] [get_bd_pins microblaze_0_xlconcat/In2]
+# connect_bd_net [get_bd_pins axi_gpio_1/ip2intc_irpt] [get_bd_pins microblaze_0_xlconcat/In3]
+# connect_bd_net [get_bd_pins axi_gpio_2/ip2intc_irpt] [get_bd_pins microblaze_0_xlconcat/In4]
 
 assign_bd_address
 save_bd_design
