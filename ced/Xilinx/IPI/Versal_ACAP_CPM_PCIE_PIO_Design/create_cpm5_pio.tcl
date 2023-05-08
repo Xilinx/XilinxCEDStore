467b407ff3ac70d235f7d5cbbbd6c7c4de13be0c
medamsindhu <sindhura.medam@amd.com>
2023-05-08 17:24:09 +0530

    CR-1160396 - Versal ACAP CPM PCIE PIO design update wrt PS PCIe Reset
    Updated PS PCIe Reset of Versal ACAP CPM PCIE PIO design

--- ced/Xilinx/IPI/Versal_ACAP_CPM_PCIE_PIO_Design/create_cpm5_pio.tcl
+++ ced/Xilinx/IPI/Versal_ACAP_CPM_PCIE_PIO_Design/create_cpm5_pio.tcl
@@ -70,43 +70,67 @@
   set pcie1_user_reset_0 [ create_bd_port -dir O -type rst pcie1_user_reset_0 ]
 
   # Create instance: versal_cips_0, and set properties
- set versal_cips_0 [ create_bd_cell -type ip -vlnv xilinx.com:ip:versal_cips versal_cips_0 ]
-  set_property -dict [ list \
-   CONFIG.BOOT_MODE {Custom} \
-   CONFIG.CPM_CONFIG {CPM_PCIE0_MODES None CPM_PCIE1_AXISTEN_IF_ENABLE_CLIENT_TAG 1\
-CPM_PCIE1_CFG_CTL_IF 1 CPM_PCIE1_CFG_EXT_IF 1 CPM_PCIE1_CFG_FC_IF 1\
-CPM_PCIE1_CFG_MGMT_IF 1 CPM_PCIE1_CFG_STS_IF 1 CPM_PCIE1_MAX_LINK_SPEED\
-16.0_GT/s CPM_PCIE1_MESG_RSVD_IF 1 CPM_PCIE1_MESG_TRANSMIT_IF 1 CPM_PCIE1_MODES\
-PCIE CPM_PCIE1_MODE_SELECTION Advanced CPM_PCIE1_MSI_X_OPTIONS MSI-X_Internal\
-CPM_PCIE1_PASID_IF 1 CPM_PCIE1_PF0_BAR0_SIZE 256 CPM_PCIE1_PF0_BAR1_ENABLED 1\
-CPM_PCIE1_PF0_BAR1_SCALE Kilobytes CPM_PCIE1_PF0_BAR2_ENABLED 1\
-CPM_PCIE1_PF0_BAR2_SCALE Kilobytes CPM_PCIE1_PF0_BAR3_ENABLED 1\
-CPM_PCIE1_PF0_BAR3_SCALE Kilobytes CPM_PCIE1_PF0_BAR4_ENABLED 1\
-CPM_PCIE1_PF0_BAR4_SCALE Kilobytes CPM_PCIE1_PF0_BAR5_ENABLED 1\
-CPM_PCIE1_PF0_BAR5_SCALE Kilobytes CPM_PCIE1_PF0_CFG_DEV_ID B03F\
-CPM_PCIE1_PF0_DEV_CAP_EXT_TAG_EN 1 CPM_PCIE1_PF0_MSIX_CAP_PBA_OFFSET 280\
-CPM_PCIE1_PF0_MSIX_CAP_TABLE_OFFSET 200 CPM_PCIE1_PL_LINK_CAP_MAX_LINK_WIDTH X8\
-CPM_PCIE1_RQ_STRADDLE_SIZE 2_TLP CPM_PCIE1_TX_FC_IF 1}\
-   CONFIG.DESIGN_MODE {1} \
-   CONFIG.PS_PMC_CONFIG {BOOT_MODE Custom DESIGN_MODE 1 PCIE_APERTURES_DUAL_ENABLE 0\
-PCIE_APERTURES_SINGLE_ENABLE 0 PMC_QSPI_PERIPHERAL_ENABLE 1\
-PMC_QSPI_PERIPHERAL_MODE {Dual Parallel} PS_BOARD_INTERFACE Custom\
-PS_PCIE1_PERIPHERAL_ENABLE 0 PS_PCIE2_PERIPHERAL_ENABLE 1 PS_PCIE_EP_RESET2_IO\
-{PMC_MIO 39} PS_PCIE_RESET {{ENABLE 1} {IO {PS_MIO 18 .. 19}}} SMON_ALARMS\
-Set_Alarms_On SMON_ENABLE_TEMP_AVERAGING 0 SMON_TEMP_AVERAGING_SAMPLES 8}\
- ] $versal_cips_0
+  set versal_cips_0 [ create_bd_cell -type ip -vlnv xilinx.com:ip:versal_cips versal_cips_0 ]
+  set_property -dict [list \
+    CONFIG.BOOT_MODE {Custom} \
+    CONFIG.CPM_CONFIG { \
+      CPM_PCIE0_MODES {None} \
+      CPM_PCIE1_AXISTEN_IF_ENABLE_CLIENT_TAG {1} \
+      CPM_PCIE1_CFG_CTL_IF {1} \
+      CPM_PCIE1_CFG_EXT_IF {1} \
+      CPM_PCIE1_CFG_FC_IF {1} \
+      CPM_PCIE1_CFG_MGMT_IF {1} \
+      CPM_PCIE1_CFG_STS_IF {1} \
+      CPM_PCIE1_MAX_LINK_SPEED {16.0_GT/s} \
+      CPM_PCIE1_MESG_RSVD_IF {1} \
+      CPM_PCIE1_MESG_TRANSMIT_IF {1} \
+      CPM_PCIE1_MODES {PCIE} \
+      CPM_PCIE1_MODE_SELECTION {Advanced} \
+      CPM_PCIE1_MSI_X_OPTIONS {MSI-X_Internal} \
+      CPM_PCIE1_PASID_IF {1} \
+      CPM_PCIE1_PF0_BAR0_SIZE {256} \
+      CPM_PCIE1_PF0_BAR1_ENABLED {1} \
+      CPM_PCIE1_PF0_BAR1_SCALE {Kilobytes} \
+      CPM_PCIE1_PF0_BAR2_ENABLED {1} \
+      CPM_PCIE1_PF0_BAR2_SCALE {Kilobytes} \
+      CPM_PCIE1_PF0_BAR3_ENABLED {1} \
+      CPM_PCIE1_PF0_BAR3_SCALE {Kilobytes} \
+      CPM_PCIE1_PF0_BAR4_ENABLED {1} \
+      CPM_PCIE1_PF0_BAR4_SCALE {Kilobytes} \
+      CPM_PCIE1_PF0_BAR5_ENABLED {1} \
+      CPM_PCIE1_PF0_BAR5_SCALE {Kilobytes} \
+      CPM_PCIE1_PF0_CFG_DEV_ID {B03F} \
+      CPM_PCIE1_PF0_DEV_CAP_EXT_TAG_EN {1} \
+      CPM_PCIE1_PF0_MSIX_CAP_PBA_OFFSET {280} \
+      CPM_PCIE1_PF0_MSIX_CAP_TABLE_OFFSET {200} \
+      CPM_PCIE1_PL_LINK_CAP_MAX_LINK_WIDTH {X8} \
+      CPM_PCIE1_RQ_STRADDLE_SIZE {2_TLP} \
+      CPM_PCIE1_TX_FC_IF {1} \
+    } \
+    CONFIG.DESIGN_MODE {1} \
+    CONFIG.PS_PMC_CONFIG { \
+      BOOT_MODE {Custom} \
+      DESIGN_MODE {1} \
+      PCIE_APERTURES_DUAL_ENABLE {0} \
+      PCIE_APERTURES_SINGLE_ENABLE {0} \
+      PMC_OSPI_PERIPHERAL {{ENABLE 0} {IO {PMC_MIO 0 .. 11}} {MODE Single}} \
+      PMC_QSPI_PERIPHERAL_ENABLE {1} \
+      PMC_QSPI_PERIPHERAL_MODE {Dual Parallel} \
+      PS_BOARD_INTERFACE {Custom} \
+      PS_PCIE1_PERIPHERAL_ENABLE {0} \
+      PS_PCIE2_PERIPHERAL_ENABLE {1} \
+      PS_PCIE_EP_RESET1_IO {None} \
+      SMON_ALARMS {Set_Alarms_On} \
+      SMON_ENABLE_TEMP_AVERAGING {0} \
+      SMON_TEMP_AVERAGING_SAMPLES {0} \
+    } \
+  ] $versal_cips_0
 
-# Changing boot mode to QSPI
- set_property -dict [list \
-   CONFIG.PS_PMC_CONFIG { BOOT_MODE Custom  \
-                          PMC_OSPI_PERIPHERAL {{ENABLE 0} {IO {PMC_MIO 0 .. 11}} {MODE Single}}  \
-			  PMC_QSPI_PERIPHERAL_ENABLE 1  \
-			  PMC_QSPI_PERIPHERAL_MODE {Dual Parallel}  \
-			  SMON_ALARMS Set_Alarms_On  \
-			  SMON_ENABLE_TEMP_AVERAGING 0  \
-			  SMON_TEMP_AVERAGING_SAMPLES 8 } \
-   CONFIG.BOOT_MODE {Custom} \
- ] $versal_cips_0 
+set board_part [get_property NAME [current_board_part]]
+if [regexp "vpk120_es:part0:1.3" $board_part] {
+set_property CONFIG.PS_PMC_CONFIG { PS_PCIE_EP_RESET2_IO {PMC_MIO 39} PS_PCIE_RESET {{ENABLE 1}} } [get_bd_cells versal_cips_0]
+} else {
+set_property CONFIG.PS_PMC_CONFIG { PS_PCIE_EP_RESET2_IO {PS_MIO 19} PS_PCIE_RESET {{ENABLE 1}} } [get_bd_cells versal_cips_0] }
 
   # Create interface connections
   connect_bd_intf_net -intf_net gt_refclk1_0_1 [get_bd_intf_ports gt_refclk1_0] [get_bd_intf_pins versal_cips_0/gt_refclk1]
@@ -129,12 +153,12 @@ Set_Alarms_On SMON_ENABLE_TEMP_AVERAGING 0 SMON_TEMP_AVERAGING_SAMPLES 8}\
   # Create port connections
   connect_bd_net -net cpm_irq0_0_1 [get_bd_ports cpm_irq0_0] [get_bd_pins versal_cips_0/cpm_irq0]
   connect_bd_net -net cpm_irq1_0_1 [get_bd_ports cpm_irq1_0] [get_bd_pins versal_cips_0/cpm_irq1]
-  connect_bd_net -net versal_cips_0_cpm_cor_irq [get_bd_ports cpm_cor_irq_0] [get_bd_pins versal_cips_0/cpm_cor_irq]
-  connect_bd_net -net versal_cips_0_cpm_misc_irq [get_bd_ports cpm_misc_irq_0] [get_bd_pins versal_cips_0/cpm_misc_irq]
-  connect_bd_net -net versal_cips_0_cpm_uncor_irq [get_bd_ports cpm_uncor_irq_0] [get_bd_pins versal_cips_0/cpm_uncor_irq]
-  connect_bd_net -net versal_cips_0_pcie1_user_clk [get_bd_ports pcie1_user_clk_0] [get_bd_pins versal_cips_0/pcie1_user_clk]
-  connect_bd_net -net versal_cips_0_pcie1_user_lnk_up [get_bd_ports pcie1_user_lnk_up_0] [get_bd_pins versal_cips_0/pcie1_user_lnk_up]
-  connect_bd_net -net versal_cips_0_pcie1_user_reset [get_bd_ports pcie1_user_reset_0] [get_bd_pins versal_cips_0/pcie1_user_reset]
+  connect_bd_net -net versal_cips_0_cpm_cor_irq [get_bd_pins versal_cips_0/cpm_cor_irq] [get_bd_ports cpm_cor_irq_0]
+  connect_bd_net -net versal_cips_0_cpm_misc_irq [get_bd_pins versal_cips_0/cpm_misc_irq] [get_bd_ports cpm_misc_irq_0]
+  connect_bd_net -net versal_cips_0_cpm_uncor_irq [get_bd_pins versal_cips_0/cpm_uncor_irq] [get_bd_ports cpm_uncor_irq_0]
+  connect_bd_net -net versal_cips_0_pcie1_user_clk [get_bd_pins versal_cips_0/pcie1_user_clk] [get_bd_ports pcie1_user_clk_0]
+  connect_bd_net -net versal_cips_0_pcie1_user_lnk_up [get_bd_pins versal_cips_0/pcie1_user_lnk_up] [get_bd_ports pcie1_user_lnk_up_0]
+  connect_bd_net -net versal_cips_0_pcie1_user_reset [get_bd_pins versal_cips_0/pcie1_user_reset] [get_bd_ports pcie1_user_reset_0]
 
   # Create address segments
 
