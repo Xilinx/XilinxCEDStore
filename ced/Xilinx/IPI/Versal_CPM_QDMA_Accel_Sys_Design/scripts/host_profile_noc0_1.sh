5e12e3a220e815ca06a5089050c52b00108cf48c
Srinadh Utukuru <venkata.srinadh.utukuru@amd.com>
2025-04-21 17:44:20 -0600

    Create host_profile_noc0_1.sh
    Host side script to program host profile registers of CPM5-QDMA

---
+++ ced/Xilinx/IPI/Versal_CPM_QDMA_Accel_Sys_Design/scripts/host_profile_noc0_1.sh
@@ -0,0 +1,36 @@
+/usr/local/sbin/dma-ctl qdma01000 reg write bar 0 0x2c8 0x0
+/usr/local/sbin/dma-ctl qdma01000 reg write bar 0 0x308 0x0
+
+/usr/local/sbin/dma-ctl qdma01000 reg write bar 0 0x824 0xffffffff 
+/usr/local/sbin/dma-ctl qdma01000 reg write bar 0 0x828 0xffffffff 
+/usr/local/sbin/dma-ctl qdma01000 reg write bar 0 0x82C 0xffffffff 
+/usr/local/sbin/dma-ctl qdma01000 reg write bar 0 0x830 0xffffffff 
+/usr/local/sbin/dma-ctl qdma01000 reg write bar 0 0x834 0xffffffff 
+/usr/local/sbin/dma-ctl qdma01000 reg write bar 0 0x838 0xffffffff 
+/usr/local/sbin/dma-ctl qdma01000 reg write bar 0 0x83C 0xffffffff 
+/usr/local/sbin/dma-ctl qdma01000 reg write bar 0 0x840 0xffffffff 
+
+/usr/local/sbin/dma-ctl qdma01000 reg write bar 0 0x804 0x0 
+/usr/local/sbin/dma-ctl qdma01000 reg write bar 0 0x808 0x0
+/usr/local/sbin/dma-ctl qdma01000 reg write bar 0 0x80C 0x00000000
+/usr/local/sbin/dma-ctl qdma01000 reg write bar 0 0x810 0x0
+/usr/local/sbin/dma-ctl qdma01000 reg write bar 0 0x814 0x0
+/usr/local/sbin/dma-ctl qdma01000 reg write bar 0 0x818 0x00000000
+/usr/local/sbin/dma-ctl qdma01000 reg write bar 0 0x81C 0x0
+/usr/local/sbin/dma-ctl qdma01000 reg write bar 0 0x820 0x0
+
+/usr/local/sbin/dma-ctl qdma01000 reg write bar 0 0x844 0x34
+
+/usr/local/sbin/dma-ctl qdma01000 reg write bar 0 0x2c8 0x0
+/usr/local/sbin/dma-ctl qdma01000 reg write bar 0 0x308 0x0
+
+/usr/local/sbin/dma-ctl qdma01000 reg write bar 0 0x804 0x0 
+/usr/local/sbin/dma-ctl qdma01000 reg write bar 0 0x808 0x0
+/usr/local/sbin/dma-ctl qdma01000 reg write bar 0 0x80C 0x40000000
+/usr/local/sbin/dma-ctl qdma01000 reg write bar 0 0x810 0x0
+/usr/local/sbin/dma-ctl qdma01000 reg write bar 0 0x814 0x0
+/usr/local/sbin/dma-ctl qdma01000 reg write bar 0 0x818 0x00040000
+/usr/local/sbin/dma-ctl qdma01000 reg write bar 0 0x81C 0x0
+/usr/local/sbin/dma-ctl qdma01000 reg write bar 0 0x820 0x0
+
+/usr/local/sbin/dma-ctl qdma01000 reg write bar 0 0x844 0xb4
